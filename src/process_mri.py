232a5dc38f6d49cd5cc96aa1635661e01a337687
aonghus <aonghuslawlor@gmail.com>
2021-08-14 11:01:41 +0100

    change file naming

--- src/process_mri.py
+++ src/process_mri.py
@@ -31,37 +31,38 @@ def process_bet(source_file=None, source_suffix=".nii.gz", target_suffix="_bet.n
 
     target_file = Path(source_file.parents[0] / source_file.name.replace(source_suffix, target_suffix))
     cmd = ["bet", source_file.as_posix(), target_file.as_posix(), *BET_FLAGS.split(' ')]
-    print(cmd)
+    log.info(f"{' '.join(cmd)}")
     subprocess.call(cmd)
     return target_file
 
 
-def process_reorient(source_file=None, source_suffix="_bet.nii.gz", target_suffix="_reorient.nii.gz"):
+def process_reorient(source_file=None, source_suffix=".nii.gz", target_suffix="_reorient.nii.gz"):
     FSLREORIENT2DSTD_FLAGS = get_config().FSLREORIENT2DSTD_FLAGS
     target_file = Path(source_file.parents[0] / source_file.name.replace(source_suffix, target_suffix))
 
     cmd = ["fslreorient2std", source_file.as_posix(), target_file.as_posix(), *FSLREORIENT2DSTD_FLAGS.split(' ')]
-    print(cmd)
+    log.info(f"{' '.join(cmd)}")
     subprocess.call(cmd)
     return target_file
 
 
-def process_registration(source_file=None, source_suffix="_reorient.nii.gz", target_suffix="_registered.nii.gz"):
+def process_registration(source_file=None, source_suffix=".nii.gz", target_suffix="_registered.nii.gz"):
     FLIRT_FLAGS = get_config().FLIRT_FLAGS
     REFERENCE_TEMPLATE = get_config().REFERENCE_TEMPLATE
     target_file = Path(source_file.parents[0] / source_file.name.replace(source_suffix, target_suffix))
 
     cmd = ["flirt", "-in", source_file.as_posix(), "-out", target_file.as_posix(), "-ref", REFERENCE_TEMPLATE,
            *FLIRT_FLAGS.split(' ')]
-    print(cmd)
+    log.info(f"{' '.join(cmd)}")
     subprocess.call(cmd)
     return target_file
 
 
 def process_pipeline(source_file):
-    source_file = process_bet(source_file, ".nii.gz", "_bet.nii.gz")
-    source_file = process_reorient(source_file, "_bet.nii.gz", "_reorient.nii.gz")
-    source_file = process_registration(source_file, "_reorient.nii.gz", "_registered.nii.gz")
+
+    source_file = process_bet(source_file, "_bet.nii.gz")
+    source_file = process_reorient(source_file, "_reorient.nii.gz")
+    source_file = process_registration(source_file, "_registered.nii.gz")
     return
 
 
@@ -72,7 +73,7 @@ def main():
     futures = []
     with ProcessPoolExecutor() as executor:
         for source_file in files:
-            print('working on', source_file)
+            log.info('working on', source_file)
             # process_pipeline(source_file)
             futures.append(executor.submit(process_pipeline, source_file))
 
